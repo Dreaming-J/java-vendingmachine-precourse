68b5519dd3097ececaa0ff9fa1ecadb5ac308d79
Dreaming-J <kimjjong321@naver.com>
2023-12-01 14:17:05 +0900

    :white_check_mark: test. 자판기 재고 생성 테스트

---
+++ src/test/java/vendingmachine/model/StockGeneratorTest.java
@@ -0,0 +1,16 @@
+package vendingmachine.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class StockGeneratorTest {
+
+    @ParameterizedTest(name = "[{index}] {0}")
+    @ValueSource(strings = {"[콜라,1500,]", "[콜라,,10]", "[,1500,10]", "(콜라,1500,10)", "[콜라,1500,10],[사이다:1000,20]"})
+    void 재고_생성_테스트(String data) {
+        assertThatThrownBy(() -> StockGenerator.generateStock(data))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
